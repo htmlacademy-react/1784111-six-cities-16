697431cc8c3a65ee246af8c102e3a0a5dd4263e0
SergeyY1994 <serrrrrrrrr@yandex.ru>
2024-07-11 16:49:09 +0300

    добавляет роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import Page404 from '../../pages/page-404/page-404';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   offerCardCount: number;
@@ -6,7 +13,36 @@ type AppScreenProps = {
 
 function App({offerCardCount}: AppScreenProps): JSX.Element {
   return (
-    <MainPage offerCardCount={offerCardCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage offerCardCount={offerCardCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path='*'
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-export const Setting = {
-  offerCardCount: 5,
-} as const;
+import { Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
